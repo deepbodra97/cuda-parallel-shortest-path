44531c6e06192217426600f08d46c430540b3489
deepbodra96 <deepbodra96@gmail.com>
2021-03-18 11:47:03 -0400

    gpu bellman ford naive works

--- CpuBellmanFord.cu
+++ CpuBellmanFord.cu
@@ -43,105 +43,101 @@ void bellmanFord(int src, int numVertex, int* vertices, int* indices, int* edges
 }
 
 
-int main() {
-
-    /* Adjacency Matrix */
-    
-    //int h_costMatrix[6][6] = {
-    //    {INF, 1, 5, INF, INF, INF},
-    //    {INF, INF, 2, 2, 1, INF},
-    //    {INF, INF, INF, INF, 2, INF},
-    //    {INF, INF, INF, INF, 3, 1},
-    //    {INF, INF, INF, INF, INF, 2},
-    //    {INF, INF, INF, INF, INF, INF},
-    //};
-    //int numVertex = 6;
-    //int src = 1;
-
-    //int* costMatrix = (int*) malloc(numVertex * numVertex * sizeof(int));
-    //if (costMatrix == NULL) {
-    //    cout << "malloc failed" << endl;
-    //}
-    //fill(costMatrix, costMatrix + numVertex * numVertex, INF);
-
-    //for (int i = 0; i < numVertex; i++) {
-    //    for (int j = 0; j < numVertex; j++) {
-    //        costMatrix[i * numVertex + j] = h_costMatrix[i][j];
-    //    }
-    //}
-
-    //// fileToCostMatrix(string("nyc-d.txt"), numVertex, costMatrix);
-
-    //int* parent = (int*) malloc(numVertex * sizeof(int));
-    //int* distance = (int*) malloc(numVertex * sizeof(int));
-
-    //fill(distance, distance + numVertex, INF);
-    //fill(parent, parent + numVertex, -1);
-
-    //bellmanFord(numVertex, src, (int*)costMatrix, distance, parent);
-    //printPathSSSP(numVertex, distance, parent);
-
-    /* Adjacency Linked List */
-    /*int numVertex = 264346;
-    int src = 1;
-
-    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
-    
-    graph->numVertex = numVertex;
-    graph->neighbors = (struct AdjacencyList*) malloc(numVertex * sizeof(struct AdjacencyList));
-    
-    for (int i = 0; i < numVertex; ++i) {
-        graph->neighbors[i].head = NULL;
-    }
-
-    graph = fileToAdjacencyList(string("nyc-d.txt"), graph);
-
-
-    int* costMatrix = (int*)malloc(numVertex * numVertex * sizeof(int));
-    if (costMatrix == NULL) {
-        cout << "malloc failed" << endl;
-    }
-    fill(costMatrix, costMatrix + numVertex * numVertex, INF);
-
-    int* parent = (int*)malloc(numVertex * sizeof(int));
-    int* distance = (int*)malloc(numVertex * sizeof(int));
-    bool* visited = (bool*)malloc(numVertex * sizeof(bool));
-
-    fill(distance, distance + numVertex, INF);
-    fill(visited, visited + numVertex, false);
-    fill(parent, parent + numVertex, -1);
-
-    dijkstra(graph, src, visited, distance, parent);
-    printPathSSSP(numVertex, distance, parent);*/
-
-    int src = 0;
-
-    int numVertex, numEdges;
-    map<int, list< pair<int, int > > > adjacencyList;
-    
-    fileToAdjacencyList(string("nyc-d.txt"), adjacencyList, numVertex, numEdges);
-
-    // vector<int> vertices = { 0, 1, 2, 3, 4, 5 }, indices = { 0, 2, 5, 6, 8, 9 }, edges = { 1, 2, 2, 3, 4, 4, 4, 5, 5 }, weights = { 1,5,2,2,1,2,3,1,2 };
-    vector<int> vertices, indices, edges, weights;
-    /*vertices.reserve(numVertex);
-    indices.reserve(numVertex + 1);
-    edges.reserve(numEdges);
-    weights.reserve(numEdges);*/
-
-    adjacencyListToCSR(adjacencyList, vertices, indices, edges, weights);
-    
-    int* parent = (int*)malloc(numVertex * sizeof(int));
-    int* distance = (int*)malloc(numVertex * sizeof(int));
-
-    fill(distance, distance + numVertex, INF);
-    fill(parent, parent + numVertex, -1);
-    cout << "before" << endl;
-
-    /*for (int i = 0; i < numEdges; i++) {
-        std::cout << weights[i] << ' ';
-    }*/
-
-    bellmanFord(src, numVertex, vertices.data(), indices.data(), edges.data(), weights.data(), distance, parent);
-    cout << "after" << endl;
-    printPathSSSP(numVertex, distance, parent);
-}
+//int main() {
+//
+//    /* Adjacency Matrix */
+//    
+//    //int h_costMatrix[6][6] = {
+//    //    {INF, 1, 5, INF, INF, INF},
+//    //    {INF, INF, 2, 2, 1, INF},
+//    //    {INF, INF, INF, INF, 2, INF},
+//    //    {INF, INF, INF, INF, 3, 1},
+//    //    {INF, INF, INF, INF, INF, 2},
+//    //    {INF, INF, INF, INF, INF, INF},
+//    //};
+//    //int numVertex = 6;
+//    //int src = 1;
+//
+//    //int* costMatrix = (int*) malloc(numVertex * numVertex * sizeof(int));
+//    //if (costMatrix == NULL) {
+//    //    cout << "malloc failed" << endl;
+//    //}
+//    //fill(costMatrix, costMatrix + numVertex * numVertex, INF);
+//
+//    //for (int i = 0; i < numVertex; i++) {
+//    //    for (int j = 0; j < numVertex; j++) {
+//    //        costMatrix[i * numVertex + j] = h_costMatrix[i][j];
+//    //    }
+//    //}
+//
+//    //// fileToCostMatrix(string("nyc-d.txt"), numVertex, costMatrix);
+//
+//    //int* parent = (int*) malloc(numVertex * sizeof(int));
+//    //int* distance = (int*) malloc(numVertex * sizeof(int));
+//
+//    //fill(distance, distance + numVertex, INF);
+//    //fill(parent, parent + numVertex, -1);
+//
+//    //bellmanFord(numVertex, src, (int*)costMatrix, distance, parent);
+//    //printPathSSSP(numVertex, distance, parent);
+//
+//    /* Adjacency Linked List */
+//    /*int numVertex = 264346;
+//    int src = 1;
+//
+//    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+//    
+//    graph->numVertex = numVertex;
+//    graph->neighbors = (struct AdjacencyList*) malloc(numVertex * sizeof(struct AdjacencyList));
+//    
+//    for (int i = 0; i < numVertex; ++i) {
+//        graph->neighbors[i].head = NULL;
+//    }
+//
+//    graph = fileToAdjacencyList(string("nyc-d.txt"), graph);
+//
+//
+//    int* costMatrix = (int*)malloc(numVertex * numVertex * sizeof(int));
+//    if (costMatrix == NULL) {
+//        cout << "malloc failed" << endl;
+//    }
+//    fill(costMatrix, costMatrix + numVertex * numVertex, INF);
+//
+//    int* parent = (int*)malloc(numVertex * sizeof(int));
+//    int* distance = (int*)malloc(numVertex * sizeof(int));
+//    bool* visited = (bool*)malloc(numVertex * sizeof(bool));
+//
+//    fill(distance, distance + numVertex, INF);
+//    fill(visited, visited + numVertex, false);
+//    fill(parent, parent + numVertex, -1);
+//
+//    dijkstra(graph, src, visited, distance, parent);
+//    printPathSSSP(numVertex, distance, parent);*/
+//
+//    int src = 0;
+//
+//    int numVertex, numEdges;
+//    map<int, list< pair<int, int > > > adjacencyList;
+//    
+//    fileToAdjacencyList(string("nyc-d.txt"), adjacencyList, numVertex, numEdges);
+//
+//    // vector<int> vertices = { 0, 1, 2, 3, 4, 5 }, indices = { 0, 2, 5, 6, 8, 9 }, edges = { 1, 2, 2, 3, 4, 4, 4, 5, 5 }, weights = { 1,5,2,2,1,2,3,1,2 };
+//    vector<int> vertices, indices, edges, weights;
+//    /*vertices.reserve(numVertex);
+//    indices.reserve(numVertex + 1);
+//    edges.reserve(numEdges);
+//    weights.reserve(numEdges);*/
+//
+//    adjacencyListToCSR(adjacencyList, vertices, indices, edges, weights);
+//    
+//    int* parent = (int*)malloc(numVertex * sizeof(int));
+//    int* distance = (int*)malloc(numVertex * sizeof(int));
+//
+//    fill(distance, distance + numVertex, INF);
+//    fill(parent, parent + numVertex, -1);
+//    cout << "before" << endl;
+//
+//    bellmanFord(src, numVertex, vertices.data(), indices.data(), edges.data(), weights.data(), distance, parent);
+//    cout << "after" << endl;
+//    printPathSSSP(numVertex, distance, parent);
+//}
--- GpuBellmanFord.cu
+++ GpuBellmanFord.cu
@@ -1,76 +1,147 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#include "cudaCheck.cuh"
+
 #include <iostream>
 
 #include "utils.h"
 
 using namespace std;
 
+__global__
+void bellmanFordRelax(int numVertex, int* vertices, int* indices, int* edges, int* weights, int* prev_distance, int* distance, int* parent) {
+
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (tid < numVertex) {
+        for (int j = indices[tid]; j < indices[tid + 1]; j++) {
+            int v = edges[j];
+            int w = weights[j];
+
+            if (prev_distance[tid] != INF && (prev_distance[tid] + w) < prev_distance[v]) {
+                 // parent[v] = i; // atomic
+                atomicMin(&distance[v], prev_distance[tid] + w);
+            }
+        }
+        /*if (prev_distance[tid] > distance[tid]) {
+            prev_distance[tid] = distance[tid];
+        }*/
+        prev_distance[tid] = distance[tid];
+    }
+}
+
 //__global__
-//void bellmanFord(int numVertex, int src, int* costMatrix, int* distance, int* parent) {
-//    distance[src] = 0;
+//void bellmanFordUpdateDistance() {
 //
-//    for (int i = 0; i < numVertex - 1; i++) {
-//        for (int u = 0; u < numVertex; u++) {
-//            for (int v = 0; v < numVertex; v++) {
-//                if (costMatrix[u * numVertex + v] != INF && distance[u] != INF && (distance[u] + costMatrix[u * numVertex + v]) < distance[v]) {
-//                    parent[v] = u;
-//                    distance[v] = distance[u] + costMatrix[u * numVertex + v];
-//                }
-//            }
-//        }
-//    }
 //}
 
-//int main() {
-//
-//    ///* Adjacency Matrix */
-//    //int h_costMatrix[6][6] = {
-//    //    {INF, 1, 5, INF, INF, INF},
-//    //    {INF, INF, 2, 2, 1, INF},
-//    //    {INF, INF, INF, INF, 2, INF},
-//    //    {INF, INF, INF, INF, 3, 1},
-//    //    {INF, INF, INF, INF, INF, 2},
-//    //    {INF, INF, INF, INF, INF, INF},
-//    //};
-//    //int numVertex = 6;
-//    //int src = 1;
-//
-//    //int* costMatrix = (int*)malloc(numVertex * numVertex * sizeof(int));
-//    //if (costMatrix == NULL) {
-//    //    cout << "malloc failed" << endl;
-//    //}
-//    //fill(costMatrix, costMatrix + numVertex * numVertex, INF);
-//
-//    //for (int i = 0; i < numVertex; i++) {
-//    //    for (int j = 0; j < numVertex; j++) {
-//    //        costMatrix[i * numVertex + j] = h_costMatrix[i][j];
-//    //    }
-//    //}
-//
-//    //// fileToCostMatrix(string("nyc-d.txt"), numVertex, costMatrix);
-//
-//    //int* parent = (int*)malloc(numVertex * sizeof(int));
-//    //int* distance = (int*)malloc(numVertex * sizeof(int));
-//
-//    //fill(distance, distance + numVertex, INF);
-//    //fill(parent, parent + numVertex, -1);
-//
-//    //bellmanFord(numVertex, src, (int*)costMatrix, distance, parent);
-//    //printPathSSSP(numVertex, distance, parent);
-//
-//    int numVertex, numEdges;
-//    map<int, list< pair<int, int > > > adjacencyList;
-//
-//    fileToAdjacencyList(string("nyc-d.txt"), adjacencyList, numVertex, numEdges);
-//    // cout << adjacencyList.size() << " " << numVertex << " " << numEdges << endl;
-//    vector<int> vertices, indices, edges, weights;
-//    /*vertices.reserve(numVertex);
-//    indices.reserve(numVertex + 1);
-//    edges.reserve(numEdges);
-//    weights.reserve(numEdges);*/
-//    adjacencyListToCSR(adjacencyList, vertices, indices, edges, weights);
-//    /*for (auto i : weights)
-//        std::cout << i << ' ';*/
-//}
+int main() {
+
+    ///* Adjacency Matrix */
+    //int h_costMatrix[6][6] = {
+    //    {INF, 1, 5, INF, INF, INF},
+    //    {INF, INF, 2, 2, 1, INF},
+    //    {INF, INF, INF, INF, 2, INF},
+    //    {INF, INF, INF, INF, 3, 1},
+    //    {INF, INF, INF, INF, INF, 2},
+    //    {INF, INF, INF, INF, INF, INF},
+    //};
+    //int numVertex = 6;
+    //int src = 1;
+
+    //int* costMatrix = (int*)malloc(numVertex * numVertex * sizeof(int));
+    //if (costMatrix == NULL) {
+    //    cout << "malloc failed" << endl;
+    //}
+    //fill(costMatrix, costMatrix + numVertex * numVertex, INF);
+
+    //for (int i = 0; i < numVertex; i++) {
+    //    for (int j = 0; j < numVertex; j++) {
+    //        costMatrix[i * numVertex + j] = h_costMatrix[i][j];
+    //    }
+    //}
+
+    //// fileToCostMatrix(string("nyc-d.txt"), numVertex, costMatrix);
+
+    //int* parent = (int*)malloc(numVertex * sizeof(int));
+    //int* distance = (int*)malloc(numVertex * sizeof(int));
+
+    //fill(distance, distance + numVertex, INF);
+    //fill(parent, parent + numVertex, -1);
+
+    //bellmanFord(numVertex, src, (int*)costMatrix, distance, parent);
+    //printPathSSSP(numVertex, distance, parent);
+
+    // int numVertex, numEdges;
+    // map<int, list< pair<int, int > > > adjacencyList;
+
+    // fileToAdjacencyList(string("nyc-d.txt"), adjacencyList, numVertex, numEdges);
+    // cout << adjacencyList.size() << " " << numVertex << " " << numEdges << endl;
+    // vector<int> vertices, indices, edges, weights;
+    /*vertices.reserve(numVertex);
+    indices.reserve(numVertex + 1);
+    edges.reserve(numEdges);
+    weights.reserve(numEdges);*/
+    // adjacencyListToCSR(adjacencyList, vertices, indices, edges, weights);
+    /*for (auto i : weights)
+        std::cout << i << ' ';*/
+
+    // int numVertex = 6, numEdges = 9;
+    // vector<int> vertices = { 0, 1, 2, 3, 4, 5 }, indices = { 0, 2, 5, 6, 8, 9 }, edges = { 1, 2, 2, 3, 4, 4, 4, 5, 5 }, weights = { 1,5,2,2,1,2,3,1,2 };
+    int numVertex, numEdges;
+    vector<int> vertices, indices, edges, weights;
+    map<int, list< pair<int, int > > > adjacencyList;    
+    fileToAdjacencyList(string("nyc-d.txt"), adjacencyList, numVertex, numEdges);
+    adjacencyListToCSR(adjacencyList, vertices, indices, edges, weights);
+
+    int src = 0;
+
+    int* parent = (int*)malloc(numVertex * sizeof(int));
+    int* prev_distance = (int*)malloc(numVertex * sizeof(int));
+    int* distance = (int*)malloc(numVertex * sizeof(int));
+
+    
+    fill(prev_distance, prev_distance + numVertex, INF);
+    fill(distance, distance + numVertex, INF);
+    fill(parent, parent + numVertex, -1);
+
+    prev_distance[src] = 0;
+    distance[src] = 0;
+
+    int* d_vertices;
+    int* d_indices;
+    int* d_edges;
+    int* d_weights;
+    int* d_prev_distance;
+    int* d_distance;
+    int* d_parent;
+    
+    cudaCheck(cudaMalloc((void**)&d_vertices, numVertex*sizeof(int)));
+    cudaCheck(cudaMalloc((void**)&d_indices, (numVertex+1) * sizeof(int)));
+    cudaCheck(cudaMalloc((void**)&d_edges, numEdges * sizeof(int)));
+    cudaCheck(cudaMalloc((void**)&d_weights, numEdges * sizeof(int)));
+
+    cudaCheck(cudaMalloc((void**)&d_prev_distance, numVertex * sizeof(int)));
+    cudaCheck(cudaMalloc((void**)&d_distance, numVertex * sizeof(int)));
+    cudaCheck(cudaMalloc((void**)&d_parent, numVertex * sizeof(int)));
+
+    cudaCheck(cudaMemcpy(d_vertices, vertices.data(), numVertex * sizeof(int), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(d_indices, indices.data(), (numVertex+1) * sizeof(int), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(d_edges, edges.data(), numEdges * sizeof(int), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(d_weights, weights.data(), numEdges * sizeof(int), cudaMemcpyHostToDevice));
+
+    cudaCheck(cudaMemcpy(d_prev_distance, prev_distance, numVertex * sizeof(int), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(d_distance, distance, numVertex * sizeof(int), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(d_parent, parent, numVertex * sizeof(int), cudaMemcpyHostToDevice));
+
+    for (int k = 0; k < numVertex - 1; k++) {
+        bellmanFordRelax<<<(numVertex-1)/512 + 1, 512>>>(numVertex, d_vertices, d_indices, d_edges, d_weights, d_prev_distance, d_distance, d_parent);
+    }
+
+    cudaCheck(cudaMemcpy(distance, d_distance, numVertex * sizeof(int), cudaMemcpyDeviceToHost));
+
+    for (int i = 0; i < numVertex; i++) {
+        cout<<distance[i]<<" ";
+    }
+}
